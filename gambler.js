80191f1da8303153e2b8dc3c9a8a740c61130686
Meciek <maciasmax10@gmail.com>
2022-02-08 16:48:30 +0100

    next

--- gambler.js
+++ gambler.js
@@ -1,48 +1,72 @@
 $(document).ready(function () {
     
+    const select = new Audio("sounds/select.wav");
+
+    var picked_songs = [];
+
     function roll(n) {
         $.getJSON("songs.json", function(data) {
-            
             var song = data[getOnlyId(n, data)];
             song = "<p id='" + n + "'>" + song.name + "</p>";
             var id = '#' + n;
-
-            //console.log(song1, id);
-
             $(id).replaceWith(song);
         });
     }
 
     function roll_everything() {
-        var timesRun = 0;
-        var interval1 = setInterval(function(){
-            roll(1);
-            roll(2);
-            roll(3);
-            roll(4);
-            timesRun += 1;
-            if(timesRun === 40) {
-                clearInterval(interval1);
-                const audio = new Audio("sound.wav");
-                audio.play();
-            }
-        }, 111); 
 
+        select.play();
+        
+        var timesRun = 0;
+        const end = new Audio("sounds/end.wav");
+
+        var interval1 = setInterval(function() {
+            roll(1);
+            if(timesRun >= 30) clearInterval(interval1);
+        }, 50);
+        
+        var interval2 = setInterval(function() {
+            roll(2);
+            if(timesRun >= 37) clearInterval(interval2);
+        }, 50); 
+
+        var interval3 = setInterval(function() {
+            roll(3);
+            if(timesRun >= 44) clearInterval(interval3);
+        }, 50); 
+
+        var interval4 = setInterval(function() {
+            timesRun += 0.5;
+            roll(4);
+            if(timesRun >= 51) {
+                clearInterval(interval4);
+                end.play();
+            }
+        }, 50); 
     }
 
-    document.getElementById("start").addEventListener("click", roll_everything);
-
     function getOnlyId(n, data) {
-
         var index = Math.floor(Math.random() * data.length);
         var index2 = Math.floor(Math.random() * data[index].nr.length);
 
-        if(data[index].nr[index2] != n) return getOnlyId(n, data);
-        
-        //console.log(data[index].nr[index2]);
+        if(data[index].nr[index2] != n || 
+            picked_songs.includes(data[index])) return getOnlyId(n, data);
 
+        picked_songs[n-1] = data[index];
+        //console.log(picked_songs);
         return index;
     }
+
+    var enableSubmit = function(ele) {
+        $(ele).removeAttr("disabled");
+    }
+    
+    $("#start").click(function() {
+        var that = this;
+        roll_everything();
+        $(this).attr("disabled", true);
+        setTimeout(function() { enableSubmit(that) }, 6000);
+    });
 
 });
 
