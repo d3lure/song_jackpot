508a3a9d0e86fcdd72217e5183c6bec00aa5db4a
Meciek <maciasmax10@gmail.com>
2022-02-08 01:16:45 +0100

    yes

--- gambler.js
+++ gambler.js
@@ -1,3 +1,47 @@
-function list_items() {
+$(document).ready(function () {
     
-}
+    function roll(n) {
+        $.getJSON("songs.json", function(data) {
+            
+            var song = data[getOnlyId(n, data)];
+            song = "<p id='" + n + "'>" + song.name + "</p>";
+            var id = '#' + n;
+
+            //console.log(song1, id);
+
+            $(id).replaceWith(song);
+        });
+    }
+
+    function roll_everything() {
+        var timesRun = 0;
+        var interval1 = setInterval(function(){
+            roll(1);
+            roll(2);
+            roll(3);
+            roll(4);
+            timesRun += 1;
+            if(timesRun === 40) {
+                clearInterval(interval1);
+            }
+        }, 111); 
+
+    }
+
+    document.getElementById("start").addEventListener("click", roll_everything);
+
+    function getOnlyId(n, data) {
+
+        var index = Math.floor(Math.random() * data.length);
+        var index2 = Math.floor(Math.random() * data[index].nr.length);
+
+        if(data[index].nr[index2] != n) return getOnlyId(n, data);
+        
+        //console.log(data[index].nr[index2]);
+
+        return index;
+    }
+
+});
+
+
